eaaa93f13ca9eadbfa10712d3f0e0b8c3a9b49b2
asprin107 <asprin107@gmail.com>
2023-06-08 17:41:57 +0900

    Add k6 code prometheus output

--- k6/build/build-k6.sh
+++ k6/build/build-k6.sh
@@ -3,7 +3,8 @@
 # Build k6 binary
 # For linux
 #docker run --rm -it -u "$(id -u):$(id -g)" -v "${PWD}:/xk6" grafana/xk6 build v0.43.1 \
-#  --with github.com/grafana/xk6-output-influxdb@v0.4.0
+#  --with github.com/grafana/xk6-output-influxdb@v0.4.0 \
+#  --with github.com/grafana/xk6-output-prometheus-remote@v0.2.1
 
 # For MacOS
 docker run --rm -it -e GOOS=darwin -u "$(id -u):$(id -g)" -v "${PWD}:/xk6" grafana/xk6 build v0.43.1 \
